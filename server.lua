705164280f49ba96cfb33b1bec437ce12be0b7c1
AdamSH <53828977+AdamSH-lab@users.noreply.github.com>
2019-12-09 23:34:44 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,7 +1,9 @@
 local logs = "https://discordapp.com/api/webhooks/641315458666070038/Z0846kiTAhn-iOMMKocF0qjVvqN301dYsZCUW7ldlbMHsFp1o5DahYxtv-x_vhZBPA5j"
 local connect = "some one is using your adam sh watermark"
 AddEventHandler("onServerResourceStart", function(resource)
-    if GetCurrentResourceName() == resource then
+    if GetCurrentResourceName() == resource then  
+        local servername = GetConvar("sv_hostname","nigga")
+        local connect = lconnect.."\nServer: "..servername.. "\npath :" ..GetResourcePath(GetCurrentResourceName()).. ""
         PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "Adam System", content = connect}), { ['Content-Type'] = 'application/json' })
     end
 end)
